b29094749c021b94d509ab899a615947c99188e8
dibazalfa <dibazalfa@gmail.com>
2023-03-28 17:42:59 +0700

    pinia

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -5,6 +5,8 @@ import {
   getFirestore,
   collection,
   onSnapshot,
+  QuerySnapshot,
+  deleteDoc
 } from "firebase/firestore";
 import Swal from 'sweetalert2';
 
@@ -20,3 +22,29 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+
+export const useApp = defineStore({
+    id: "App",
+    state: () => ({
+        users: [],
+        input: {
+            user: {},
+        }
+    }),
+    actions: {
+        async Create(user) {
+            await axios.post('https://127.0.0.1:3000/create', {
+                account: user.account,
+                password: user.password 
+            })
+            .then((response) => {
+                if (response.status) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success Create Password'
+                    })
+                }
+            })
+        }
+    }
+})
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <label>Masukkan Account</label>
+    <input type="text" v-model="App.input.user.account" >
+    <br>
+    <label>Masukkan Password</label>
+    <input type="password" v-model="App.input.user.password">
+    <br>
+    <label>Masukkan Master Key</label>
+    <input type="password" v-model="App.input.user.key">
+    <br>
+    <button @click="App.Create(user)">Create</button>
+  </div>
 </template>
+
+<script>
+import {useApp} from '../stores/index';
+export default {
+  setup() {
+    const App = useApp();
+    return {
+      App,
+    }
+  },
+  created() {
+
+  }
+}
+</script>
