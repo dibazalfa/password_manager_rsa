7651bcb38cf84466ee56afe9cd1b4e7230d0e235
alweismiau <keizafina@gmail.com>
2023-03-28 20:36:18 +0700

    fe dikit

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,303 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- NAVBAR -->
+  <div class="sticky top-0 z-40">
+    <div
+      class="w-full h-20 px-6 bg-gray-100 border-b flex items-center justify-between"
+    >
+      <!-- left navbar -->
+      <div class="flex">
+        <!-- mobile hamburger -->
+        <div class="inline-block lg:hidden flex items-center mr-4">
+          <button
+            class="hover:text-blue-500 hover:border-white focus:outline-none navbar-burger"
+          >
+            <svg
+              class="h-5 w-5"
+              v-bind:style="{ fill: 'black' }"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+
+        <!-- search bar -->
+        <div class="relative text-gray-600">
+          <input
+            type="search"
+            name="serch"
+            placeholder="Search products..."
+            class="bg-white h-10 w-full xl:w-64 px-5 rounded-lg border text-sm focus:outline-none"
+          />
+          <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+            <svg
+              class="h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              style="enable-background: new 0 0 56.966 56.966"
+              xml:space="preserve"
+              width="512px"
+              height="512px"
+            >
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <!-- right navbar -->
+      <div class="flex items-center relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          class="fill-current mr-3 hover:text-blue-500"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"
+          />
+        </svg>
+        <img
+          src="https://a7sas.net/wp-content/uploads/2019/07/4060.jpeg"
+          class="w-12 h-12 rounded-full shadow-lg"
+        />
+      </div>
+    </div>
+    <div
+      class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-gray-100 border-r z-30"
+      id="main-nav"
+    >
+      <!-- SIDEBAR -->
+
+      <div class="w-full h-20 border-b flex px-4 items-center mb-8">
+        <p class="font-semibold text-3xl text-blue-400 pl-4">LOGO</p>
+      </div>
+
+      <div class="mb-4 px-4">
+        <p class="pl-4 text-sm font-semibold mb-1">MAIN</p>
+        <div
+          class="w-full flex items-center text-blue-400 h-10 pl-4 bg-gray-200 hover:bg-gray-200 rounded-lg cursor-pointer"
+        >
+          <svg class="h-6 w-6 fill-current mr-2" viewBox="0 0 20 20">
+            <path
+              d="M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z"
+            ></path>
+          </svg>
+          <span class="text-gray-700">Dashboard</span>
+        </div>
+        <div
+          class="w-full flex items-center text-blue-400 h-10 pl-4 hover:bg-gray-200 rounded-lg cursor-pointer"
+        >
+          <svg class="h-6 w-6 fill-current mr-2" viewBox="0 0 20 20">
+            <path
+              d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z"
+            ></path>
+          </svg>
+          <span class="text-gray-700">Analytics</span>
+        </div>
+
+        <div
+          class="w-full flex items-center text-blue-400 h-10 pl-4 hover:bg-gray-200 rounded-lg cursor-pointer"
+        >
+          <svg class="h-6 w-6 fill-current mr-2" viewBox="0 0 20 20">
+            <path
+              d="M17.498,11.697c-0.453-0.453-0.704-1.055-0.704-1.697c0-0.642,0.251-1.244,0.704-1.697c0.069-0.071,0.15-0.141,0.257-0.22c0.127-0.097,0.181-0.262,0.137-0.417c-0.164-0.558-0.388-1.093-0.662-1.597c-0.075-0.141-0.231-0.22-0.391-0.199c-0.13,0.02-0.238,0.027-0.336,0.027c-1.325,0-2.401-1.076-2.401-2.4c0-0.099,0.008-0.207,0.027-0.336c0.021-0.158-0.059-0.316-0.199-0.391c-0.503-0.274-1.039-0.498-1.597-0.662c-0.154-0.044-0.32,0.01-0.416,0.137c-0.079,0.106-0.148,0.188-0.22,0.257C11.244,2.956,10.643,3.207,10,3.207c-0.642,0-1.244-0.25-1.697-0.704c-0.071-0.069-0.141-0.15-0.22-0.257C7.987,2.119,7.821,2.065,7.667,2.109C7.109,2.275,6.571,2.497,6.07,2.771C5.929,2.846,5.85,3.004,5.871,3.162c0.02,0.129,0.027,0.237,0.027,0.336c0,1.325-1.076,2.4-2.401,2.4c-0.098,0-0.206-0.007-0.335-0.027C3.001,5.851,2.845,5.929,2.77,6.07C2.496,6.572,2.274,7.109,2.108,7.667c-0.044,0.154,0.01,0.32,0.137,0.417c0.106,0.079,0.187,0.148,0.256,0.22c0.938,0.936,0.938,2.458,0,3.394c-0.069,0.072-0.15,0.141-0.256,0.221c-0.127,0.096-0.181,0.262-0.137,0.416c0.166,0.557,0.388,1.096,0.662,1.596c0.075,0.143,0.231,0.221,0.392,0.199c0.129-0.02,0.237-0.027,0.335-0.027c1.325,0,2.401,1.076,2.401,2.402c0,0.098-0.007,0.205-0.027,0.334C5.85,16.996,5.929,17.154,6.07,17.23c0.501,0.273,1.04,0.496,1.597,0.66c0.154,0.047,0.32-0.008,0.417-0.137c0.079-0.105,0.148-0.186,0.22-0.256c0.454-0.453,1.055-0.703,1.697-0.703c0.643,0,1.244,0.25,1.697,0.703c0.071,0.07,0.141,0.15,0.22,0.256c0.073,0.098,0.188,0.152,0.307,0.152c0.036,0,0.073-0.004,0.109-0.016c0.558-0.164,1.096-0.387,1.597-0.66c0.141-0.076,0.22-0.234,0.199-0.393c-0.02-0.129-0.027-0.236-0.027-0.334c0-1.326,1.076-2.402,2.401-2.402c0.098,0,0.206,0.008,0.336,0.027c0.159,0.021,0.315-0.057,0.391-0.199c0.274-0.5,0.496-1.039,0.662-1.596c0.044-0.154-0.01-0.32-0.137-0.416C17.648,11.838,17.567,11.77,17.498,11.697 M16.671,13.334c-0.059-0.002-0.114-0.002-0.168-0.002c-1.749,0-3.173,1.422-3.173,3.172c0,0.053,0.002,0.109,0.004,0.166c-0.312,0.158-0.64,0.295-0.976,0.406c-0.039-0.045-0.077-0.086-0.115-0.123c-0.601-0.6-1.396-0.93-2.243-0.93s-1.643,0.33-2.243,0.93c-0.039,0.037-0.077,0.078-0.116,0.123c-0.336-0.111-0.664-0.248-0.976-0.406c0.002-0.057,0.004-0.113,0.004-0.166c0-1.75-1.423-3.172-3.172-3.172c-0.054,0-0.11,0-0.168,0.002c-0.158-0.312-0.293-0.639-0.405-0.975c0.044-0.039,0.085-0.078,0.124-0.115c1.236-1.236,1.236-3.25,0-4.486C3.009,7.719,2.969,7.68,2.924,7.642c0.112-0.336,0.247-0.664,0.405-0.976C3.387,6.668,3.443,6.67,3.497,6.67c1.75,0,3.172-1.423,3.172-3.172c0-0.054-0.002-0.11-0.004-0.168c0.312-0.158,0.64-0.293,0.976-0.405C7.68,2.969,7.719,3.01,7.757,3.048c0.6,0.6,1.396,0.93,2.243,0.93s1.643-0.33,2.243-0.93c0.038-0.039,0.076-0.079,0.115-0.123c0.336,0.112,0.663,0.247,0.976,0.405c-0.002,0.058-0.004,0.114-0.004,0.168c0,1.749,1.424,3.172,3.173,3.172c0.054,0,0.109-0.002,0.168-0.004c0.158,0.312,0.293,0.64,0.405,0.976c-0.045,0.038-0.086,0.077-0.124,0.116c-0.6,0.6-0.93,1.396-0.93,2.242c0,0.847,0.33,1.645,0.93,2.244c0.038,0.037,0.079,0.076,0.124,0.115C16.964,12.695,16.829,13.021,16.671,13.334 M10,5.417c-2.528,0-4.584,2.056-4.584,4.583c0,2.529,2.056,4.584,4.584,4.584s4.584-2.055,4.584-4.584C14.584,7.472,12.528,5.417,10,5.417 M10,13.812c-2.102,0-3.812-1.709-3.812-3.812c0-2.102,1.71-3.812,3.812-3.812c2.102,0,3.812,1.71,3.812,3.812C13.812,12.104,12.102,13.812,10,13.812"
+            ></path>
+          </svg>
+          <span class="text-gray-700">Settings</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- HOME -->
+
+    <div id="home">
+      <!-- breadcrumb -->
+      <nav class="text-sm font-semibold mb-6" aria-label="Breadcrumb">
+        <ol class="list-none p-0 inline-flex">
+          <li class="flex items-center text-blue-500">
+            <a href="#" class="text-gray-700">Home</a>
+            <svg
+              class="fill-current w-3 h-3 mx-3"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <path
+                d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+              />
+            </svg>
+          </li>
+          <li class="flex items-center">
+            <a href="#" class="text-gray-600">Dashboard</a>
+          </li>
+        </ol>
+      </nav>
+      <!-- breadcrumb end -->
+
+      <div class="lg:flex justify-between items-center mb-6">
+        <p class="text-2xl font-semibold mb-2 lg:mb-0">Good afternoon, Joe!</p>
+        <button
+          class="bg-blue-500 hover:bg-blue-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow"
+        >
+          View Logs
+        </button>
+      </div>
+
+      <div class="flex flex-wrap -mx-3 mb-20">
+        <div class="w-1/2 xl:w-1/4 px-3">
+          <div
+            class="w-full bg-white border text-blue-400 rounded-lg flex items-center p-6 mb-6 xl:mb-0"
+          >
+            <svg
+              class="w-16 h-16 fill-current mr-4 hidden lg:block"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.35,2.219h-5.934c-0.115,0-0.225,0.045-0.307,0.128l-8.762,8.762c-0.171,0.168-0.171,0.443,0,0.611l5.933,5.934c0.167,0.171,0.443,0.169,0.612,0l8.762-8.763c0.083-0.083,0.128-0.192,0.128-0.307V2.651C17.781,2.414,17.587,2.219,17.35,2.219M16.916,8.405l-8.332,8.332l-5.321-5.321l8.333-8.332h5.32V8.405z M13.891,4.367c-0.957,0-1.729,0.772-1.729,1.729c0,0.957,0.771,1.729,1.729,1.729s1.729-0.772,1.729-1.729C15.619,5.14,14.848,4.367,13.891,4.367 M14.502,6.708c-0.326,0.326-0.896,0.326-1.223,0c-0.338-0.342-0.338-0.882,0-1.224c0.342-0.337,0.881-0.337,1.223,0C14.84,5.826,14.84,6.366,14.502,6.708"
+              ></path>
+            </svg>
+
+            <div class="text-gray-700">
+              <p class="font-semibold text-3xl">237</p>
+              <p>Products Sold</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-1/2 xl:w-1/4 px-3">
+          <div
+            class="w-full bg-white border text-blue-400 rounded-lg flex items-center p-6 mb-6 xl:mb-0"
+          >
+            <svg
+              class="w-16 h-16 fill-current mr-4 hidden lg:block"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.684,7.925l-5.131-0.67L10.329,2.57c-0.131-0.275-0.527-0.275-0.658,0L7.447,7.255l-5.131,0.67C2.014,7.964,1.892,8.333,2.113,8.54l3.76,3.568L4.924,17.21c-0.056,0.297,0.261,0.525,0.533,0.379L10,15.109l4.543,2.479c0.273,0.153,0.587-0.089,0.533-0.379l-0.949-5.103l3.76-3.568C18.108,8.333,17.986,7.964,17.684,7.925 M13.481,11.723c-0.089,0.083-0.129,0.205-0.105,0.324l0.848,4.547l-4.047-2.208c-0.055-0.03-0.116-0.045-0.176-0.045s-0.122,0.015-0.176,0.045l-4.047,2.208l0.847-4.547c0.023-0.119-0.016-0.241-0.105-0.324L3.162,8.54L7.74,7.941c0.124-0.016,0.229-0.093,0.282-0.203L10,3.568l1.978,4.17c0.053,0.11,0.158,0.187,0.282,0.203l4.578,0.598L13.481,11.723z"
+              ></path>
+            </svg>
+
+            <div class="text-gray-700">
+              <p class="font-semibold text-3xl">177</p>
+              <p>Product Reviews</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-1/2 xl:w-1/4 px-3">
+          <div
+            class="w-full bg-white border text-blue-400 rounded-lg flex items-center p-6"
+          >
+            <svg
+              class="w-16 h-16 fill-current mr-4 hidden lg:block"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M14.999,8.543c0,0.229-0.188,0.417-0.416,0.417H5.417C5.187,8.959,5,8.772,5,8.543s0.188-0.417,0.417-0.417h9.167C14.812,8.126,14.999,8.314,14.999,8.543 M12.037,10.213H5.417C5.187,10.213,5,10.4,5,10.63c0,0.229,0.188,0.416,0.417,0.416h6.621c0.229,0,0.416-0.188,0.416-0.416C12.453,10.4,12.266,10.213,12.037,10.213 M14.583,6.046H5.417C5.187,6.046,5,6.233,5,6.463c0,0.229,0.188,0.417,0.417,0.417h9.167c0.229,0,0.416-0.188,0.416-0.417C14.999,6.233,14.812,6.046,14.583,6.046 M17.916,3.542v10c0,0.229-0.188,0.417-0.417,0.417H9.373l-2.829,2.796c-0.117,0.116-0.71,0.297-0.71-0.296v-2.5H2.5c-0.229,0-0.417-0.188-0.417-0.417v-10c0-0.229,0.188-0.417,0.417-0.417h15C17.729,3.126,17.916,3.313,17.916,3.542 M17.083,3.959H2.917v9.167H6.25c0.229,0,0.417,0.187,0.417,0.416v1.919l2.242-2.215c0.079-0.077,0.184-0.12,0.294-0.12h7.881V3.959z"
+              ></path>
+            </svg>
+
+            <div class="text-gray-700">
+              <p class="font-semibold text-3xl">31</p>
+              <p>New Enquiries</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-1/2 xl:w-1/4 px-3">
+          <div
+            class="w-full bg-white border text-blue-400 rounded-lg flex items-center p-6"
+          >
+            <svg
+              class="w-16 h-16 fill-current mr-4 hidden lg:block"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z"
+              ></path>
+            </svg>
+
+            <div class="text-gray-700">
+              <p class="font-semibold text-3xl">1,653</p>
+              <p>Product Views</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-wrap -mx-3">
+        <div class="w-full xl:w-1/3 px-3">
+          <p class="text-xl font-semibold mb-4">Recent Sales</p>
+
+          <div class="w-full bg-white border rounded-lg p-4 mb-8 xl:mb-0">
+            <canvas id="buyers-chart" width="600" height="400"></canvas>
+          </div>
+        </div>
+
+        <div class="w-full xl:w-1/3 px-3">
+          <p class="text-xl font-semibold mb-4">Recent Reviews</p>
+
+          <div class="w-full bg-white border rounded-lg p-4 mb-8 xl:mb-0">
+            <canvas id="reviews-chart" width="600" height="400"></canvas>
+          </div>
+        </div>
+
+        <div class="w-full xl:w-1/3 px-3">
+          <p class="text-xl font-semibold mb-4">Recent Transactions</p>
+          <div class="w-full bg-white border rounded-lg p-4">
+            <div
+              class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2 mb-4"
+            >
+              <div>
+                <p class="font-semibold text-xl">Trent Murphy</p>
+                <p>Product 1</p>
+              </div>
+              <span class="text-green-500 font-semibold text-lg">$25.00</span>
+            </div>
+
+            <div
+              class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2 mb-4"
+            >
+              <div>
+                <p class="font-semibold text-xl">Joseph Brent</p>
+                <p>Product 34</p>
+              </div>
+              <span class="text-red-500 font-semibold text-lg">$74.99</span>
+            </div>
+
+            <div
+              class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2 mb-4"
+            >
+              <div>
+                <p class="font-semibold text-xl">Jacob Bator</p>
+                <p>Product 23</p>
+              </div>
+              <span class="text-green-500 font-semibold text-lg">$14.95</span>
+            </div>
+
+            <div
+              class="w-full bg-gray-100 border rounded-lg flex justify-between items-center px-4 py-2"
+            >
+              <div>
+                <p class="font-semibold text-xl">Alex Mason</p>
+                <p>Product 66</p>
+              </div>
+              <span class="text-green-500 font-semibold text-lg">$44.99</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
